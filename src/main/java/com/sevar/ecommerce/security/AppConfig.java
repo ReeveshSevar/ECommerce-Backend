bfec1a8c6952dd39baa3a577ed05b10e23bf7aed
Reevesh Sevar <110165835+ReeveshSevar@users.noreply.github.com>
2024-12-19 17:18:50 +0530

    AppConfig.java

--- src/main/java/com/sevar/ecommerce/security/AppConfig.java
+++ src/main/java/com/sevar/ecommerce/security/AppConfig.java
@@ -43,8 +43,10 @@ public class AppConfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration cfg = new CorsConfiguration();
-                        cfg.setAllowedOrigins(Arrays.asList(
-                                "https://sevariya.vercel.app"
+                         cfg.setAllowedOrigins(Arrays.asList(
+                                "https://sevariya.vercel.app",                             
+                                "http://localhost:3001",
+                                 "http://localhost:3000"
                         ));
                         cfg.setAllowedMethods(Collections.singletonList("*"));
                         cfg.setAllowCredentials(true);
